3892b98c89c6ed4fec946fe5b3944d8dc7809332
Jake Gallow <jakegallow@microsoft.com>
2023-04-17 10:52:09 -0700

    scale factors

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use tokio::runtime::Runtime;
 #[derive(Debug, Parser)]
 struct Cli {
     /// The number of tokio instances
-    max_tokio_instances: u16,
+    max_tokio_instances: u32,
 
     /// the factor by which to scale up to the max_tokio_instances
     /// so if max_tokio_instances is 1000 and the scale factor is 10
@@ -14,7 +14,7 @@ struct Cli {
     scale_factor: u8,
 
     /// The number of axum servers per instance of tokio
-    servers_per_instance: u16,
+    servers_per_instance: u32,
 
     /// true to use axum (webserver). false to just no op every second
     /// axum seems to freak out once you start 255 servers (on linux, untested on windows)
@@ -26,34 +26,32 @@ fn main() {
     println!("running simulation with this cli: {cli:?}");
     println!("========================");
 
-    let mut current_instance_num = 1;
-
-    while current_instance_num <= cli.max_tokio_instances {
-        if cli.use_axum {
-            println!("running simulation with {current_instance_num} tokio instances and {} axum instance per runtime", cli.servers_per_instance);
-        } else {
-            println!("running simulation with {current_instance_num} tokio instances and printing \"\" every second");
-        }
+    let mut current_target = 1;
+    'Simulation: loop {
         let mut current_port_num = 3000_u16;
         let mut rts = vec![];
-        for active_runtimes in 0..current_instance_num {
+        for num_runtimes in 0..current_target {
             start_new_runtime_instance(
                 cli.servers_per_instance,
                 &mut current_port_num,
                 &mut rts,
                 cli.use_axum,
             );
-            println!("current number of active runtime: {}", active_runtimes + 1);
+            println!("current number of active runtime: {}", num_runtimes + 1);
+            if num_runtimes >= cli.max_tokio_instances {
+                // let it run for a while for profiling
+                std::thread::sleep(std::time::Duration::from_secs(60));
+                println!("Simulation complete");
+                break 'Simulation;
+            }
         }
 
         // let it run for a while for profiling
         std::thread::sleep(std::time::Duration::from_secs(60));
 
         println!("simulation complete. scaling up instances");
-        current_instance_num *= 10;
         println!("------------------------------------------");
-
-        println!("Simulation complete");
+        rts.clear();
     }
 
     // just dropping everything is fine
@@ -63,7 +61,7 @@ fn main() {
 /// The task will be an axum web server
 /// returns a vec of runtimes so nothing gets dropped
 fn start_new_runtime_instance(
-    tasks_per_instance: u16,
+    tasks_per_instance: u32,
     current_port_num: &mut u16,
     rts: &mut Vec<Runtime>,
     use_axum: bool,
